6414f904d7ea88d8587ae0c151a04681ed2a566c
Dmytro DEmontazz <demontazz@aether-project.games>
2020-11-11 21:55:11 +0000

    Update UI

--- .space.kts
+++ .space.kts
@@ -6,16 +6,16 @@
 
 job("Build and deploy web") {
     container("cirrusci/flutter") {
+        env["IP"] = Params("web_ip")
+        env["DIR"] = Params("web_dir")
+        env["KEY1"] = Secrets("web_key_1")
+        env["KEY2"] = Secrets("web_key_2")
         shellScript {
             content = """
             	flutter config --enable-web
                 flutter upgrade
                 flutter pub get
                 flutter build web
-                env["IP"] = Params("web_ip")
-                env["DIR"] = Params("web_dir")
-                env["KEY1"] = Secrets("web_key_1")
-                env["KEY2"] = Secrets("web_key_2")
                 echo ${"$"}KEY1 > key.pem
                 echo ${"$"}KEY2 >> key.pem
                 cat key.pem
