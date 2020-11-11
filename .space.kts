36d6b54b536ea4483c9e0b4b1aeb1570f8e8da2d
Dmytro DEmontazz <demontazz@aether-project.games>
2020-11-11 22:45:04 +0000

    Update CI

--- .space.kts
+++ .space.kts
@@ -17,8 +17,8 @@ job("Build and deploy web") {
                 flutter pub get
                 flutter build web
                 touch key.pem
-                tr ' ' '\n' < ${"$"}STARTKEY${"$"}ENDKEY >> key.pem
-                tr ' ' '\n' < ${"$"}STARTKEY${"$"}ENDKEY
+                ${"$"}STARTKEY ${"$"}ENDKEY | tr " " "\n" | >> key.pem
+                cat key.pem
                 chmod 600 key.pem
                 scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i key.pem build/web root@${"$"}IP:${"$"}DIR
             """
