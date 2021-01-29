340fc39dd23bbd4026a57972b81608341ae0015a
demontazz <demontazz@aether-project.games>
2021-01-29 19:39:29 +0200

    Updated CI

--- .space.kts
+++ .space.kts
@@ -6,6 +6,9 @@
 
 job("Build and deploy web") {
     container("cirrusci/flutter") {
+        env["BUILD_IP"] = Params("web_build_ip")
+        env["BUILD_USERNAME"] = Params("web_build_username")
+        env["BUILD_PASSWORD"] = Secrets("web_build_password")
         env["IP"] = Params("web_ip")
         env["DIR"] = Params("web_dir")
         env["STARTKEY"] = Secrets("web_key_1")
@@ -13,16 +16,24 @@ job("Build and deploy web") {
         env["newString"] = ""
         shellScript {
             content = """
+                flutter channel beta
             	flutter config --enable-web
                 flutter upgrade
                 flutter pub get
                 flutter build web --release --dart-define=FLUTTER_WEB_USE_EXPERIMENTAL_CANVAS_TEXT=true
+                flutter build apk --release
                 touch key.pem
                 echo ${"$"}STARTKEY ${"$"}ENDKEY | sed 's/ /\n/g;w key.pem'  > /dev/null 2>&1
                 sed -i '1s/^/-----BEGIN OPENSSH PRIVATE KEY-----\n/' key.pem
                 echo "\n-----END OPENSSH PRIVATE KEY-----" >> key.pem
                 chmod 600 key.pem
                 scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i key.pem -r build/web/* root@${"$"}IP:${"$"}DIR
+                ftp -n -i ${"$"}BUILD_IP <<EOF
+                user ${"$"}BUILD_USERNAME ${"$"}BUILD_PASSWORD
+                cd /files
+                put build/app/outputs/flutter-apk/app-release.apk app-release.apk
+                quit
+                EOF
             """
         }
     }
