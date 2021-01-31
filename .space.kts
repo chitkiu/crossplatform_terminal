adaf441479e703def3b0ad9b4d8c4355571fbed8
demontazz <demontazz@aether-project.games>
2021-01-31 04:16:10 +0200

    Updated CI

--- .space.kts
+++ .space.kts
@@ -20,24 +20,6 @@ job("Build and deploy compiled app") {
         }
     }
 
-    container("occitech/ssh-client") {
-        env["IP"] = Params("web_ip")
-        env["DIR"] = Params("web_dir")
-        env["STARTKEY"] = Secrets("web_key_1")
-        env["ENDKEY"] = Secrets("web_key_2")
-
-        shellScript {
-            content = """
-                touch key.pem
-                echo ${"$"}STARTKEY ${"$"}ENDKEY | sed 's/ /\n/g;w key.pem'  > /dev/null 2>&1
-                sed -i '1s/^/-----BEGIN OPENSSH PRIVATE KEY-----\n/' key.pem
-                echo "\n-----END OPENSSH PRIVATE KEY-----" >> key.pem
-                chmod 600 key.pem
-                scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i key.pem -r $mountDir/share/web/* root@${"$"}IP:${"$"}DIR
-            """
-        }
-    }
-
     container("dockito/ftp-client") {
         env["BUILD_IP"] = Params("web_build_ip")
         env["BUILD_USERNAME"] = Params("web_build_username")
@@ -51,6 +33,25 @@ job("Build and deploy compiled app") {
                 quit
                 EOF
                 """
+        }
+    }
+
+    container("occitech/ssh-client") {
+        env["IP"] = Params("web_ip")
+        env["DIR"] = Params("web_dir")
+        env["STARTKEY"] = Secrets("web_key_1")
+        env["ENDKEY"] = Secrets("web_key_2")
+
+        shellScript {
+            content = """
+                touch key.pem
+                echo ${"$"}STARTKEY ${"$"}ENDKEY | sed 's/ /\n/g;w key.pem'
+                 #> /dev/null 2>&1
+                sed -i '1s/^/-----BEGIN OPENSSH PRIVATE KEY-----\n/' key.pem
+                echo "\n-----END OPENSSH PRIVATE KEY-----" >> key.pem
+                chmod 600 key.pem
+                scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i key.pem -r $mountDir/share/web/* root@${"$"}IP:${"$"}DIR
+            """
         }
     }
 }
